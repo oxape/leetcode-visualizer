e08ae5c13a8a2b9861bbb28770f7890b582697cf
donglu <donglu@starot.com>
2022-03-02 09:25:20 +0800

    webpack配置添加index页面,使输出目录和调试目录保持一致结构

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ const isProMode = process.env.NODE_ENV === 'production'
 module.exports = {
     mode: isProMode ? 'production' : 'development',
     entry: {
+        index: path.resolve(__dirname, 'js/index.js'),
         3: path.resolve(__dirname, 'js/3.js'),
         hello: path.resolve(__dirname, 'js/hello.js'),
     },
@@ -17,14 +18,20 @@ module.exports = {
     devtool: 'inline-source-map',
     plugins: [
         new HtmlWebpackPlugin({
+            title: 'index.html Development',
+            filename: "index.html",
+            template: path.resolve(__dirname, 'index.html'),
+            chunks:['index']
+        }),
+        new HtmlWebpackPlugin({
             title: '3.html Development',
-            filename: "3.html",
+            filename: "pages/3.html",
             template: path.resolve(__dirname, 'pages/3.html'),
             chunks:['3']
         }),
         new HtmlWebpackPlugin({
             title: 'hello.html Development',
-            filename: "hello.html",
+            filename: "pages/hello.html",
             template: path.resolve(__dirname, 'pages/hello.html'),
             chunks:['hello']
         }),
